550298824f6224445bcd2d63ea3dbe5af2a8a1b8
wonho <wonho.kang@smartcareworks.com>
2020-12-02 15:31:03 +0900

    녹스 바탕화면 감지를 Art of War 아이콘 감지로 변경

--- ArtOfHassan/MainWindow.xaml.cs
+++ ArtOfHassan/MainWindow.xaml.cs
@@ -167,14 +167,12 @@ namespace ArtOfHassan
         }
 
 
-        int NoxWallpaperX   = 100;
-        int NoxWallpaperY   = 865;
         int AppLocationX    = 60;
         int AppLocationY    = 500;
 
-        int HomeButtonX = 290;
-        int ShopButtonX = 65;
-        int ShopButtonY = 980;
+        int HomeButtonX     = 290;
+        int ShopButtonX     = 65;
+        int ShopButtonY     = 980;
 
         int MiddleButtonX   = 195;
         int MiddleButtonY   = 680;
@@ -269,9 +267,10 @@ namespace ArtOfHassan
 
 
                 // Off Check
-                System.Drawing.Color color = CurrentBitmap.GetPixel(NoxWallpaperX, NoxWallpaperY);
+                System.Drawing.Color color = CurrentBitmap.GetPixel(AppLocationX, AppLocationY);
                 TimerLog("Off Check Color: " + color.R + "," + color.G + "," + color.B);
-                if ((color.R == 47) && (color.G == 46) && (color.B == 72))
+                if (((color.R == 169) && (color.G == 216) && (color.B == 255)) ||
+                    ((color.R == 151) && (color.G == 198) && (color.B == 1)))
                 {
                     ClickLog("Off Check");
                     System.Windows.Forms.Cursor.Position = new System.Drawing.Point((int)NoxPointX + AppLocationX, (int)NoxPointY + AppLocationY);
