238b68fa8613a28641c0ff3efe1c6209465a163c
wonho <wonho.kang@smartcareworks.com>
2021-01-13 14:09:40 +0900

    SettingWindow 추가

---
+++ ArtOfHassan/SettingWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ArtOfHassan
+{
+    /// <summary>
+    /// SettingWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SettingWindow : Window
+    {
+        public SettingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AdsCloseClickPatternButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EmailTestButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DefaultButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            int caretIndex = (e.Source as TextBox).CaretIndex;
+            string name = (e.Source as TextBox).Name;
+            if (name.Contains("AppPlayerTitle"))
+            {
+                (e.Source as TextBox).Text = Regex.Replace((e.Source as TextBox).Text, RegExClass.Location, "");
+            }
+            else if (name.Contains("Email"))
+            {
+                (e.Source as TextBox).Text = Regex.Replace((e.Source as TextBox).Text, RegExClass.Email, "");
+            }
+            else
+            {
+                (e.Source as TextBox).Text = Regex.Replace((e.Source as TextBox).Text, RegExClass.Number, "");
+            }
+            (e.Source as TextBox).Select(caretIndex, 0);
+        }
+    }
+}
